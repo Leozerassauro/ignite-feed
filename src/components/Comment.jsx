825f82d4c1189f16e719c2d70c83a18c0ca56afd
Leonardo Girardi <leo.girardi97@hotmail.com>
2022-09-26 13:11:15 -0300

    Like Button Incremented

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Trash, ThumbsUp } from "phosphor-react";
 
 import { Avatar } from "./Avatar";
@@ -5,9 +6,14 @@ import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount +1);
   }
 
   return (
@@ -31,9 +37,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
